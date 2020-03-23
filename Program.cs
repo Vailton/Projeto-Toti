a025099e42b93d8d568350bcdb494009549ca00b
Vailton Carlitos Gomes <55075180+Vailton@users.noreply.github.com>
2020-03-23 20:20:13 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Internal;
+using System;
+
+namespace Projeto_Toti1
+{
+    class Calculadora
+    {
+        static void Main(string[] args)
+        {
+          
+                Console.WriteLine("Digite um nome");
+            string nome = Console.ReadLine();
+
+
+            Console.ReadKey();
+        }
+    }
+}
